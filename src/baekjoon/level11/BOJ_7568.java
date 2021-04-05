62fcc1f2c9970f37f8de7d2a053b352a1aa17aa4
dmkim <ddmkim94@gmail.com>
2021-04-05 20:39:29 +0900

    [BOJ, 7568] 덩치

--- src/baekjoon/level11/BOJ_7568.java
+++ src/baekjoon/level11/BOJ_7568.java
@@ -6,13 +6,16 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// 덩치
+// Test Case Print: 2 2 1 2 5
 public class BOJ_7568 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int rank[] = new int[N]; // 덩치랭크 count
-        Arrays.fill(rank, 1);
+
+        int[] rank = new int[N]; // 덩치랭크 count
+        Arrays.fill(rank, 1); // 배열 값은 1로 채움
 
         int[][] person = new int[N][2]; // 키, 몸무게 value
 
@@ -25,16 +28,18 @@ public class BOJ_7568 {
         }
 
         int[] result = tankRank(person, rank);
+        for(int x : result) {
+            System.out.print(x + " ");
+        }
 
     }
 
     // counting method !!
     static int[] tankRank(int[][] person, int[] rank){
-        int length = person.length; // 5
-        for(int i=0; i<length; i++) {
-            for(int j=i+1; j<length; j++) {
-                for(int k=0; k<2; k++){
-                    System.out.println(person[i][k] + "," + person[j][k]);
+        for(int i=0; i<person.length; i++){
+            for(int j=0; j<person.length; j++){
+                if(person[j][0] > person[i][0] && person[j][1] > person[i][1]){
+                    rank[i]++;
                 }
             }
         }
