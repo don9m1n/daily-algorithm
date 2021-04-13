b98a24dbd2cbba55ea226862911f0c400dfeda41
dmkim <ddmkim94@gmail.com>
2021-04-13 12:16:09 +0900

    [BOJ, 11651] 좌표 정렬하기1

---
+++ src/baekjoon/level12/BOJ_11651.java
@@ -0,0 +1,44 @@
+package baekjoon.level12;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+// 좌표 정렬하기2
+public class BOJ_11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Integer[][] pos = new Integer[N][2];
+
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            pos[i][0] = Integer.parseInt(st.nextToken());
+            pos[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(pos, new Comparator<Integer[]>() {
+            @Override
+            public int compare(Integer[] o1, Integer[] o2) {
+                if(!o1[1].equals(o2[1])) {
+                    return o1[1] - o2[1];
+                }else {
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(Integer[] i : pos) {
+            bw.write(i[0] + " " + i[1] + "\n");
+        }
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
