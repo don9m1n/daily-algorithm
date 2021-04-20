d16d8418d632cb38a6468b6be3c4111f59d73719
dmkim <ddmkim94@gmail.com>
2021-04-20 17:24:46 +0900

    중성화 여부 파악하기 update!

--- src/programmers/sql/중성화 여부 파악하기.sql
+++ src/programmers/sql/중성화 여부 파악하기.sql
@@ -1,3 +1,12 @@
+/*
+	CASE ~ THEN 문법: JAVA의 if ~ else 문법과 비슷하다고 생각!!
+	CASE 
+	WHEN '조건1' THEN '결과1'
+	WHEN '조건2' THEN '결과2'
+	ELSE '결과3'
+	END
+*/
+
 SELECT ANIMAL_ID, NAME,
     (CASE 
     WHEN SEX_UPON_INTAKE  LIKE 'Intact%' THEN 'X'
