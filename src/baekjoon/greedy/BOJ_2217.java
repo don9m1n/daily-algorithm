8f4623c723390dc88cbb45eb5d8bd13a398d00bc
dmkim <ddmkim94@gmail.com>
2021-05-15 12:51:18 +0900

    [BOJ, 2217] 로프 => 클래스 이름 update

--- src/baekjoon/greedy/BOJ_2217.java
+++ src/baekjoon/greedy/BOJ_2217.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 // 로프
-public class BOJ_2217_X {
+public class BOJ_2217 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
