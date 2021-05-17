c2d2d0e0fde469674a4243286ae692ea18598986
dmkim <ddmkim94@gmail.com>
2021-05-17 17:44:54 +0900

    [BOJ, 1744] 수 묶기 X

---
+++ src/baekjoon/greedy/BOJ_1744__X.java
@@ -0,0 +1,39 @@
+package baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// 수 묶기
+public class BOJ_1744__X {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        for(int i=0; i<N; i++) {
+            list.add(i, Integer.parseInt(br.readLine()));
+        }
+
+        list.sort(Collections.reverseOrder());
+
+        int sum = 0;
+        for(int i=0; i<list.size(); i+=2) {
+            int num1 = list.get(i);
+            int num2 = list.get(i+1);
+
+            if((num1 * num2) > (num1 + num2)) {
+                sum += (num1 * num2);
+            }else {
+                sum += (num1 + num2);
+            }
+        }
+        System.out.println(sum);
+
+
+
+    }
+}
