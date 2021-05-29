4025e2cfe4336d8cdda406c2184572d4568834b8
dmkim <ddmkim94@gmail.com>
2021-05-29 14:53:35 +0900

    [BOJ, 1924] 2007년

--- src/baekjoon/implementation/BOJ_1924.java
+++ src/baekjoon/implementation/BOJ_1924.java
@@ -3,8 +3,8 @@ package baekjoon.implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.StringTokenizer;
 
 // 2007년
@@ -18,10 +18,9 @@ public class BOJ_1924 {
 
         String[] day = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
 
-        Calendar cal = Calendar.getInstance();
-        cal.set(2007, x-1, y);
-
-        int result = cal.get(Calendar.DAY_OF_WEEK);
-        System.out.println(day[result-1]);
+        LocalDate date = LocalDate.of(2007, x, y);
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("e");
+        String result = date.format(df);
+        System.out.println(day[Integer.parseInt(result) - 1]);
     }
 }
