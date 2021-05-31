fc2d2a483e170bca951fd1af14e32416298c7762
dmkim <ddmkim94@gmail.com>
2021-05-31 13:07:09 +0900

    [BOJ, 2440, 2441] 별 찍기 - 3,4 merge

---
+++ src/baekjoon/implementation/BOJ_2441.java
@@ -0,0 +1,24 @@
+package baekjoon.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 별 찍기 - 4
+public class BOJ_2441 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<i; j++) {
+                System.out.print(" ");
+            }
+            for(int k=N; k>i; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
