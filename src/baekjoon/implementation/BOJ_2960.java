89f368909126638a673248706f2b8c28f588e411
dmkim <ddmkim94@gmail.com>
2021-06-09 15:47:57 +0900

    [BOJ, 2960] 에라토스테네스의 체 -> Refactoring

--- src/baekjoon/implementation/BOJ_2960.java
+++ src/baekjoon/implementation/BOJ_2960.java
@@ -3,9 +3,9 @@ package baekjoon.implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// 에라토스테네스의 체
 public class BOJ_2960 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,27 +15,31 @@ public class BOJ_2960 {
         int K = Integer.parseInt(st.nextToken());
         boolean[] primeNum = new boolean[N+1];
 
-        int cnt = 0;
-
         for(int i = 2; i <= N; i++) {
             primeNum[i] = true;
         }
 
+        int result = isCheck(primeNum, N, K);
+        System.out.println(result);
+    }
+
+    // Check Prime Number!
+    static int isCheck(boolean[] primeNum, int N, int K){
+        int cnt = 0;
+
         for(int i = 2; i <= N; i++) {
             for(int j = i; j <= N; j += i) {
-                // 해당 요소가 false인 경우 pass
                 if(!primeNum[j]) {
                     continue;
                 }
 
-                // true인 경우 false로 상태 변경
                 primeNum[j] = false;
                 cnt++;
                 if(cnt == K) {
-                    System.out.println(j);
-                    return;
+                    return j;
                 }
             }
         }
+        return -1;
     }
 }
