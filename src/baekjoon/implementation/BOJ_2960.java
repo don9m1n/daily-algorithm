83ffa8c5fa854f6918b8d08bc63e6f69a9df64f0
dmkim <ddmkim94@gmail.com>
2021-06-09 15:00:42 +0900

    [BOJ, 2960] 에라토스테네스의 체 -> success code 🚀

--- src/baekjoon/implementation/BOJ_2960.java
+++ src/baekjoon/implementation/BOJ_2960.java
@@ -13,32 +13,29 @@ public class BOJ_2960 {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+        boolean[] primeNum = new boolean[N+1];
 
-        int[] arr = new int[N-1];
-        for(int i = 0; i < N-1; i++) {
-            arr[i] = i + 2;
+        int cnt = 0;
+
+        for(int i = 2; i <= N; i++) {
+            primeNum[i] = true;
         }
 
-        int cnt = 0; // Counting variable
-        int result = 0;
-        while(cnt != K) {
-            int min = 0;
-            for(int i=0; i<arr.length; i++) {
-                if(arr[i] != 1) {
-                    min = arr[i];
-                    break;
+        for(int i = 2; i <= N; i++) {
+            for(int j = i; j <= N; j += i) {
+                // 해당 요소가 false인 경우 pass
+                if(!primeNum[j]) {
+                    continue;
                 }
-            }
 
-            for(int i=0; i<arr.length; i++) {
-                if(arr[i] != 1 && (arr[i] == min || arr[i] % min == 0)) {
-                    result = arr[i];
-                    arr[i] = 1;
-                    cnt++;
+                // true인 경우 false로 상태 변경
+                primeNum[j] = false;
+                cnt++;
+                if(cnt == K) {
+                    System.out.println(j);
+                    return;
                 }
             }
-            Arrays.sort(arr);
         }
-        System.out.println(result);
     }
 }
