ecb30244cd4767d3a29cca058cc8a29db039a2c8
dmkim <ddmkim94@gmail.com>
2021-06-09 15:38:05 +0900

    [BOJ, 10988] 팰린드롬인지 확인하기 -> Refactoring

--- src/baekjoon/implementation/BOJ_10988.java
+++ src/baekjoon/implementation/BOJ_10988.java
@@ -9,26 +9,37 @@ public class BOJ_10988 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] arr = br.readLine().split("");
-        String[] arr_copy = new String[arr.length];
+        String[] arr_reverse = new String[arr.length];
 
-        for(int i = arr.length-1, j = 0; i >= 0; i--, j++) {
-            arr_copy[j] = arr[i];
+        boolean check = isCheck(arr, createArr(arr, arr_reverse));
+        int result = result(check);
+        System.out.println(result);
+    }
+
+    // Create Reverse Array!
+    static String[] createArr(String[] original, String[] reverse) {
+        for(int i = original.length-1, j = 0; i >= 0; i--, j++) {
+            reverse[j] = original[i];
         }
+        return reverse;
+    }
 
+    // Check Palindrome!
+    static boolean isCheck(String[] original, String[] reverse){
         boolean check = false;
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i].equals(arr_copy[i])) {
+        for(int i = 0; i < original.length; i++) {
+            if(original[i].equals(reverse[i])) {
                 check = true;
             }else {
                 check = false;
                 break;
             }
         }
+        return check;
+    }
 
-        if(!check) {
-            System.out.println(0);
-        } else {
-            System.out.println(1);
-        }
+    // Print Result!
+    static int result(boolean check){
+        return !check ? 0 : 1;
     }
 }
