e548d2d72e599772979e538c5951e46d83370742
dmkim <ddmkim94@gmail.com>
2021-06-17 16:03:21 +0900

    [PG, 77884] 약수의 개수와 덧셈

---
+++ src/programmers/level1/PG_77884.java
@@ -0,0 +1,25 @@
+package programmers.level1;
+
+// 약수의 개수와 덧셈
+public class PG_77884 {
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        for(int i = left; i <= right; i++) {
+            int cnt = 0;
+            for(int j = 1; j <= i; j++) {
+                if(i % j == 0) {
+                    cnt++;
+                }
+            }
+
+            if(cnt % 2 == 0) {
+                answer += i;
+            }else {
+                answer -= i;
+            }
+        }
+
+        return answer;
+    }
+}
