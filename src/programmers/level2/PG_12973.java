8e47bc83f314bdd73eaec83b09c8d32c77de037f
dmkim <ddmkim94@gmail.com>
2021-06-28 22:26:57 +0900

    [PG, 12973] 짝지어 제거하기 성공^^

--- src/programmers/level2/PG_12973.java
+++ src/programmers/level2/PG_12973.java
@@ -1,31 +1,26 @@
-package programmers.level2.unsolve;
+package programmers.level2.solve;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Stack;
+
 // 짝지어 제거하기
 public class PG_12973 {
     public int solution(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        boolean isCheck = false;
+        // Hint: Stack or Queue로 풀면 시간복잡도 O(n)으로 풀 수 있음 -> 반복문으로 풀면 시간초과 남....
+        Stack<Character> stack = new Stack<>();
 
-        while(true) {
-            for (int i = 0; i < sb.length() - 1; i++) {
-                if (sb.toString().charAt(i) == sb.toString().charAt(i + 1)) {
-                    sb.delete(i, i + 2);
-                    isCheck = true;
-                    break;
-                }
-            }
-
-            if(!isCheck) {
-                return 0;
-            }else if(sb.length() == 0) {
-                break;
+        // toCharArray(): String -> char[]
+        for(char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
             }
         }
-
-        return 1;
+        // stack이 비어있으면(제거완료) 1, stack이 비어있지 않다면(제거실패) 0 return
+        return stack.isEmpty() ? 1 : 0;
     }
 
     @Test
