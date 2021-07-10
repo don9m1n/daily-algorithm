8ac4ba9f91416aab17f8fe81eaef60e6dbeb56a9
dmkim <ddmkim94@gmail.com>
2021-07-10 16:03:07 +0900

    [PG, 42577] 전화번호 목록 SUCCESS!

--- src/programmers/level2/PG_42577.java
+++ src/programmers/level2/PG_42577.java
@@ -1,30 +1,32 @@
-package programmers.level2.unsolve;
+package programmers.level2.solve;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // 전화번호 목록
 public class PG_42577 {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < phone_book.length; i++) {
+            map.put(phone_book[i], i);
+        }
 
-        // Set<String> set = new HashSet<>(Arrays.asList(phone_book));
-        for(int i = 0; i < phone_book.length-1; i++) {
-            String str = phone_book[i];
-            for(int j = i + 1; j < phone_book.length; j++) {
-                if(phone_book[j].indexOf(str) == 0) {
-                    answer = false;
-                    break;
+        for (String s : phone_book) {
+            for (int j = 1; j < s.length(); j++) {
+                if (map.containsKey(s.substring(0, j))) {
+                    return false;
                 }
             }
         }
-
-        return answer;
+        return true;
     }
 
     @Test
     public void test() {
-        String[] phone_book = {"119", "97674223", "1195524421"};
+        String[] phone_book = {"1195524421", "119", "97674223"};
         boolean result = solution(phone_book);
         Assertions.assertFalse(result);
     }
