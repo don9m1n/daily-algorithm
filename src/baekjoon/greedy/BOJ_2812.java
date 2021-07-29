7ff3845a5058584185b7d7e50de3437a62fbfe3e
dmkim <ddmkim94@gmail.com>
2021-07-29 11:43:51 +0900

    [BOJ, 2812] 크게 만들기

--- src/baekjoon/greedy/BOJ_2812.java
+++ src/baekjoon/greedy/BOJ_2812.java
@@ -1,8 +1,9 @@
-package baekjoon.greedy.unsolve;
+package baekjoon.greedy.solve;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 // 크게 만들기
@@ -14,24 +15,45 @@ public class BOJ_2812 {
         int N = Integer.parseInt(st.nextToken()); // 숫자 자릿수
         int K = Integer.parseInt(st.nextToken()); // 지울 숫자의 개수
 
-        StringBuilder sb = new StringBuilder(br.readLine());
+        String num = br.readLine();
 
-        /**
-         * 해당 자릿수부터 검사
-         * 뒷 자릿수들을 돌면서 자기보다 큰 숫자가 있으면 삭제
-         */
+        Stack<Character> stack = new Stack<>();
+        // 첫 번째 숫자는 넣고 시작
+        stack.push(num.charAt(0));
+        int cnt = 0;
+        boolean flag = true;
 
-        int flag = 0;
-        while(K > 0) {
-            for (int i = 0; i < sb.length() - 1; i++) {
-                if (sb.charAt(flag) < sb.charAt(flag + 1) || (i==0 && sb.charAt(flag) > sb.charAt(flag + 1) )) {
-                    sb.deleteCharAt(flag);
-                    K--;
-                }else {
-                    flag += 1;
+        // isEmpty(): 스택이 비어있으면 true, 아니면 false
+        for(int i = 1; i < num.length(); i++) {
+            char ch = num.charAt(i);
+
+            // 숫자를 비교하는 while문
+            while(!stack.isEmpty() && flag) {
+                if(cnt == K) {
+                    flag = false;
+                    break;
                 }
 
-                if(flag == sb.length() - 1) break;
+                if(stack.peek() < ch) {
+                    stack.pop();
+                    cnt++;
+                } else if(stack.peek() >= ch) {
+                    break;
+                }
+            }
+            stack.push(ch);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        // 반복문을 돌았지만 제거해야할 숫자 개수를 충족하지 못했을 경우
+        if(cnt < K) {
+            int gap = K - cnt;
+            for(int i = 0; i < stack.size() - gap; i++) {
+                sb.append(stack.get(i));
+            }
+        }else {
+            for(int i = 0; i < stack.size(); i++) {
+                sb.append(stack.get(i));
             }
         }
         System.out.println(sb.toString());
