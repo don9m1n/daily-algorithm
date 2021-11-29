6d909f3abce0b33147739d2eeb3de7def70a8294
dmkim <ddmkim94@gmail.com>
2021-11-29 16:31:56 +0900

    [PG, 42587] 프린터

--- src/programmers/level2/PG42587.java
+++ src/programmers/level2/PG42587.java
@@ -2,75 +2,57 @@ package programmers.level2;
 
 import java.util.*;
 
-// 프린터 <Stack, Queue>
+// 프린터 [스택, 큐]
+/**
+ * 1. 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
+ * 2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
+ * 3. 그렇지 않으면 J를 인쇄합니다.
+ */
 public class PG42587 {
 
     public static void main(String[] args) {
         int solution = solution(new int[]{2, 1, 3, 2}, 2);
-        int solution2 = PriorityQueue(new int[]{2, 1, 3, 2}, 2);
-        System.out.println("solution2 = " + solution2);
-    }
-
-    // 우선순위 큐 사용
-    public static int PriorityQueue(int[] priorities, int location){
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-
-        for (int i = 0; i < priorities.length; i++) {
-            pq.add(priorities[i]);
-        }
-
-        int koby = 0;
-        while (!pq.isEmpty()) {
-            for (int i = 0; i < priorities.length; i++) {
-                if(priorities[i] == pq.peek()) {
-                    if(location == i) {
-                        return ++koby;
-                    }
-                    pq.poll();
-                    koby++;
-                }
-            }
-        }
-        return -1;
+        System.out.println("solution = " + solution);
     }
 
     // 큐 사용
     public static int solution(int[] priorities, int location) {
 
-        Queue<Task> q = new LinkedList<>();
+        int answer = 0;
+        Queue<Document> q = new LinkedList<>(); // 우선순위 저장
+
         for (int i = 0; i < priorities.length; i++) {
-            q.add(new Task(i, priorities[i]));
+            q.add(new Document(i, priorities[i]));
         }
 
         int now = 0;
         while (!q.isEmpty()) {
-            Task current = q.poll();
+            Document curr = q.poll();
             boolean flag = false;
-            for (Task t : q) {
-                if(t.priority > current.priority) { // 현재 문서보다 중요도가 높은 문서가 큐 안에 있다면
+
+            for (Document doc : q) {
+                if(doc.priority > curr.priority) {
                     flag = true;
                 }
             }
 
-            if (flag) {
-                q.add(current);
-            } else {
+            if(flag) {
+                q.add(curr);
+            }else {
                 now++;
-                if(current.location == location) {
-                    break;
+                if(curr.location == location) {
+                    answer = now;
                 }
             }
         }
-
-        return now;
+        return answer;
     }
 
-    static class Task {
-        int location;
-        int priority;
+    static class Document {
+        private int location; // 위치
+        private int priority; // 값
 
-        public Task(int location, int priority) {
+        public Document(int location, int priority) {
             this.location = location;
             this.priority = priority;
         }
