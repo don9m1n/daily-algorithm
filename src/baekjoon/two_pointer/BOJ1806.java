ee46fb05137927a0b4ca1c6d49d52b57b5634930
dmkim <ddmkim94@gmail.com>
2022-01-04 17:26:17 +0900

    [BOJ, 1806] 부분합

--- src/baekjoon/two_pointer/BOJ1806.java
+++ src/baekjoon/two_pointer/BOJ1806.java
@@ -1,41 +1,42 @@
-package baekjoon.twopointer;
+package baekjoon.two_pointer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 // 부분합
-public class BOJ_1806 {
+public class BOJ1806 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int N = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        int s = 0;
-        int e = 0;
-        int prtlSum = 0; // 부분합
-        int min = N + 1;
+        int right = 0; // 끝점
+        int min = n + 1;
+        int sum = 0; // 부분합 저장 변수
 
-        for(int i = s; i < N; i++) {
-            while (prtlSum < S && e < N) { // 부분합이 S보다 작은경우 반복(끝점을 + 하는 경우)
-                prtlSum += arr[e++];
+        for (int left = 0; left < n; left++) {
+            while (sum < s && right < n) {
+                sum += nums[right++];
             }
-            if(prtlSum == S) {
-                min = Math.min(min, e-i);
+
+            if(sum >= s) {
+                min = Math.min(min, right - left);
             }
-            prtlSum -= arr[i];
+            sum -= nums[left];
         }
 
-
-        System.out.print(min == N + 1 ? 0 : min);
+        System.out.println(min == n + 1 ? 0 : min);
     }
 }
