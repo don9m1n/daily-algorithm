533aa3beafb764117a935db54b7d154d9bb71ac5
dmkim <ddmkim94@gmail.com>
2022-01-07 12:57:05 +0900

    [BOJ, 11399] ATM (카운팅정렬 풀이)

--- src/baekjoon/greedy/BOJ11399.java
+++ src/baekjoon/greedy/BOJ11399.java
@@ -8,24 +8,68 @@ import java.util.StringTokenizer;
 
 // ATM
 public class BOJ11399 {
+    static BufferedReader br;
+    static int n;
+    static StringTokenizer st;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
 
         int[] time = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine(), " ");
+
         for (int i = 0; i < time.length; i++) {
             time[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(time);
-        int subSum = 0;
-        int sum = 0;
-        for (int i = 0; i < time.length; i++) {
-            subSum += time[i];
-            sum += subSum;
+        int solution = normalSort(time);
+        System.out.println(countingSort(time));
+    }
+
+    private static int countingSort(int[] nums) {
+        int[] counting = new int[1001];
+        int[] result = new int[n];
+
+        // 1. 요소 카운팅
+        for (int i = 0; i < nums.length; i++) {
+            counting[nums[i]]++;
         }
 
-        System.out.println(sum);
+        // 2. 누적합 세팅
+        for (int i = 1; i < counting.length; i++) {
+            counting[i] += counting[i - 1];
+        }
+
+        // 3. result 배열 채우기
+        for (int i = n - 1; i >= 0; i--) {
+            int value = nums[i];
+            counting[value]--;
+            result[counting[value]] = value;
+        }
+
+        // 4. 누적 시간 구하기
+        int sub = 0;
+        int sum = 0;
+        for (int i = 0; i < result.length; i++) {
+            sub += result[i];
+            sum += sub;
+        }
+        return sum;
+    }
+
+
+    // Arrays.sort() 정렬
+    private static int normalSort(int[] nums) throws IOException {
+
+        Arrays.sort(nums);
+
+        int subSum = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            subSum += nums[i];
+            sum += subSum;
+        }
+        return sum;
     }
 }
