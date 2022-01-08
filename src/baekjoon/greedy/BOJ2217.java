5fa45eca69c2023eb7770716680e707572776276
dmkim <ddmkim94@gmail.com>
2022-01-08 17:05:24 +0900

    [BOJ, 2217] 로프 퀵 정렬 구현(저격 데이터 때문에 시간초과 뜸...)

--- src/baekjoon/greedy/BOJ2217.java
+++ src/baekjoon/greedy/BOJ2217.java
@@ -20,11 +20,58 @@ public class BOJ2217 {
          * 각 케이스별 들 수 있는 최대 무게
          * 가장 작은 로프 * n
          */
-        Arrays.sort(rope);
+        // 퀵 정렬!
+        m_pivot_sort(rope, 0, rope.length - 1);
+
         int max = 0;
         for (int i = n - 1; i >= 0; i--) {
             max = Math.max(max, rope[i] * (n - i));
         }
         System.out.println(max);
+    }
+
+    private static void m_pivot_sort(int[] rope, int lo, int hi) {
+
+        // lo >= hi 인 경우에는 원소가 1개 이하기 때문에 정렬할 필요가 없음
+        if (lo >= hi) {
+            return;
+        }
+
+        int pivot = partition(rope, lo, hi);
+
+        m_pivot_sort(rope, lo, pivot);
+        m_pivot_sort(rope, pivot + 1, hi);
+    }
+
+    private static int partition(int[] rope, int left, int right) {
+
+        int lo = left;
+        int hi = right;
+        int pivot = rope[((left - 1) + (right + 1)) / 2];
+
+        while (true) {
+
+            while(rope[lo] < pivot) {
+                lo++;
+            }
+
+            while(rope[hi] > pivot && lo <= hi) {
+                hi--;
+            }
+
+            // lo < hi를 만족하지 않으면 hi 리턴
+            if(lo >= hi) {
+                return hi;
+            }
+
+            // lo < hi를 만족하면 스왑
+            swap(rope, lo, hi);
+        }
+    }
+
+    private static void swap(int[] rope, int lo, int hi) {
+        int temp = rope[lo];
+        rope[lo] = rope[hi];
+        rope[hi] = temp;
     }
 }
