86f28445b14140dad1275781b38065a681453ae8
dmkim <ddmkim94@gmail.com>
2022-01-10 13:47:40 +0900

    [BOJ, 1026] 보물

---
+++ src/baekjoon/greedy/BOJ1026.java
@@ -0,0 +1,49 @@
+package baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+// 보물
+public class BOJ1026 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] a = new int[n];
+        int[] b = new int[n];
+
+        createArray(br, n, a);
+        createArray(br, n, b);
+
+        Arrays.sort(a);
+        reverseSort(b);
+
+        int s = 0;
+        for (int i = 0; i < n; i++) {
+            s += a[i] * b[i];
+        }
+
+        System.out.println(s);
+    }
+
+    private static void reverseSort(int[] arr) {
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+    }
+
+    private static void createArray(BufferedReader br, int n, int[] arr) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
