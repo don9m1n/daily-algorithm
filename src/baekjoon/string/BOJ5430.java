57bf13369817e5a6cea137960d2ca8c1d6856afc
dmkim <ddmkim94@gmail.com>
2022-01-23 23:58:26 +0900

    [BOJ, 5430] AC

--- src/baekjoon/string/BOJ5430.java
+++ src/baekjoon/string/BOJ5430.java
@@ -3,58 +3,81 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-// AC
+/**
+ * AC
+ * 숫자를 뒤집으면서 풀면 시간초과 ! -> Deque 사용
+ */
 public class BOJ5430 {
+
+    static Deque<Integer> dq = new LinkedList<>();
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < t; i++) {
-            String[] fnc = br.readLine().split("");
-            int n = Integer.parseInt(br.readLine());
-            String str = br.readLine();
-            String[] arr = str.substring(1, str.length() - 1).split(",");
 
-            if(n == 0) {
-                System.out.println("error");
+            String command = br.readLine();
+            int n = Integer.parseInt(br.readLine()); // 배열에 들어갈 숫자의 개수
+            StringTokenizer st = new StringTokenizer(br.readLine(), "[],");
+
+            dq = new LinkedList<>();
+            for (int j = 0; j < n; j++) {
+                dq.add(Integer.parseInt(st.nextToken()));
+            }
+            arrayMethod(command, dq);
+        }
+        System.out.println(sb);
+    }
+
+    private static void arrayMethod(String command, Deque<Integer> dq) throws IOException {
+
+        boolean isRight = true;
+
+        for (char cmd : command.toCharArray()) {
+            if (cmd == 'R') {
+                isRight = !isRight; // 방향 바꿔주기!
                 continue;
             }
 
-            List<Integer> list = new ArrayList<>();
-            for (int j = 0; j < arr.length; j++) {
-                list.add(Integer.parseInt(arr[j]));
-            }
-
-            boolean orderCheck = false;
-            for (int j = 0; j < fnc.length; j++) {
-                switch (fnc[j]){
-                    case "R": // 뒤집기
-                        if(!orderCheck) {
-                            list.sort(Collections.reverseOrder());
-                            orderCheck = true;
-                        }else {
-                            Collections.sort(list);
-                        }
-                        break;
-                    case "D": // 배열의 첫 번째 수 버리기
-                        if(list.size() > 0) {
-                            list.remove(0);
-                        }else {
-                            break;
-                        }
-                        break;
+            if (isRight) { // D 함수이면서 isRight = true일 경우
+                if (dq.pollFirst() == null) {
+                    sb.append("error\n");
+                    return;
+                }
+            } else {
+                if (dq.pollLast() == null) {
+                    sb.append("error\n");
+                    return;
                 }
             }
-            if (list.isEmpty()) {
-                System.out.println("error");
-            }else {
-                System.out.println(list.toString());
+        }
+        makePrintString(dq, isRight);
+    }
+
+    // 결과 메세지 만들기
+    private static void makePrintString(Deque<Integer> dq, boolean isRight) {
+
+        sb.append("[");
+
+        if (dq.size() > 0) {
+            if (isRight) {
+                sb.append(dq.pollFirst());
+
+                while (!dq.isEmpty()) {
+                    sb.append(",").append(dq.pollFirst());
+                }
+            } else {
+                sb.append(dq.pollLast());
+
+                while (!dq.isEmpty()) {
+                    sb.append(",").append(dq.pollLast());
+                }
             }
         }
+        sb.append("]").append("\n");
     }
 }
