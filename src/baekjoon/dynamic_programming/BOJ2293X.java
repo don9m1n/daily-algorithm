c8d1c1800e6fb36c0b980cbee1dbd466ebb22606
김동민 <ddmkim94@gmail.com>
2022-02-05 15:06:48 +0900

    [BOJ, 2293] 동전1 실패..

---
+++ src/baekjoon/dynamic_programming/BOJ2293X.java
@@ -0,0 +1,29 @@
+package baekjoon.dynamic_programming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 동전 1
+ */
+public class BOJ2293X {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[n];
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(coin);
+        for (int i = coin[0]; i <= k; i++) {
+
+        }
+    }
+}
