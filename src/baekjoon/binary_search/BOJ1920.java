41c822f2611aceb8e01037562145c8099171178b
ddmkim <ddmkim94@gmail.com>
2022-03-01 18:12:04 +0900

    [BOJ, 1920] 수 찾기 [2]

--- src/baekjoon/binary_search/BOJ1920.java
+++ src/baekjoon/binary_search/BOJ1920.java
@@ -1,4 +1,4 @@
-package baekjoon.binarysearch;
+package baekjoon.binary_search;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,60 +8,51 @@ import java.util.StringTokenizer;
 
 public class BOJ1920 {
 
-    static BufferedReader br;
-    static StringTokenizer st;
-    static int[] a;
-    static int[] b;
+    static int n;
+    static int[] nums;
 
     public static void main(String[] args) throws IOException {
 
-        br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
-        a = createArray(n);
-        Arrays.sort(a);
+        n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
 
         int m = Integer.parseInt(br.readLine());
-        b = createArray(m);
-
+        st = new StringTokenizer(br.readLine(), " ");
+        int[] targets = new int[m];
         for (int i = 0; i < m; i++) {
-            int target = b[i]; // 찾을 값
-            int length = a.length; // 이분 탐색을 할 배열의 길이
+            targets[i] = Integer.parseInt(st.nextToken());
+        }
 
-            System.out.println(checkNumber(target, length));
+        // 이분 탐색
+        for (int target : targets) {
+            int isCheck = binarySearch(target);
+            System.out.println(isCheck);
         }
     }
 
-    /**
-     * 이분 탐색 메서드
-     */
-    private static int checkNumber(int target, int length) {
-        int left = 0;
-        int right = length - 1;
+    private static int binarySearch(int target) {
+        int lo = 0; // 시작 지점
+        int hi = n - 1; // 끝 지점
 
-        while(left <= right) {
-            int mid = (left + right) / 2;
-
-            if(a[mid] == target) {
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2; // 중간값
+            if (target > nums[mid]) {
+                lo = mid + 1;
+            } else if (target < nums[mid]) {
+                hi = mid - 1;
+            } else { // target == nums[mid]
                 return 1;
-            } else if(a[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
             }
         }
-        return 0;
-    }
 
-    /**
-     * 배열 생성 메서드
-     */
-    private static int[] createArray(int n) throws IOException {
-        int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt((st.nextToken()));
-        }
-        return arr;
+        return 0;
     }
 }
