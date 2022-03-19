fc9a8ef5d7923bb2c9668109d7206e1f0f6f020d
ddmkim <ddmkim94@gmail.com>
2022-03-19 18:19:35 +0900

    [BOJ, 1406] 에디터(Stack 풀이 추가)

--- src/baekjoon/stack/BOJ1406.java
+++ src/baekjoon/stack/BOJ1406.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Stack;
 
 // 에디터
 public class BOJ1406 {
@@ -16,14 +17,59 @@ public class BOJ1406 {
         br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        linkedList();
+        // linkedList();
         stack();
 
     }
 
     // Stack
-    private static void stack() {
+    private static void stack() throws IOException {
+        Stack<Character> left = new Stack<>();
+        Stack<Character> right = new Stack<>();
 
+        String str = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < str.length(); i++) {
+            left.push(str.charAt(i));
+        }
+
+        for (int i = 0; i < n; i++) {
+            String command = br.readLine();
+            char c = command.charAt(0);
+
+            switch(c) {
+                case 'L':
+                    if (!left.isEmpty()) {
+                        right.push(left.pop());
+                    }
+                    break;
+                case 'D':
+                    if (!right.isEmpty()) {
+                        left.push(right.pop());
+                    }
+                    break;
+                case 'B':
+                    if (!left.isEmpty()) {
+                        left.pop();
+                    }
+                    break;
+                case 'P':
+                    left.push(command.charAt(2));
+                    break;
+            }
+        }
+
+        while (!left.isEmpty()) {
+            right.push(left.pop());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!right.isEmpty()) {
+            sb.append(right.pop());
+        }
+
+        System.out.println(sb.toString());
     }
 
     // LinkedList
