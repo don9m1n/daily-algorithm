2d6d64d199fcdb98578008fe1359b7ffc3393a42
ddmkim <ddmkim94@gmail.com>
2022-04-17 14:36:26 +0900

    [BOJ, 2164] 카드2

---
+++ src/baekjoon/queue/BOJ2164.java
@@ -0,0 +1,29 @@
+package baekjoon.queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+// 카드2
+public class BOJ2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        for (Integer q  : queue) {
+            System.out.println(q);
+        }
+    }
+}
