f98a75079bf39aed5a6d9a59d7c5d00e4ebc4edd
ddmkim <ddmkim94@gmail.com>
2022-06-14 16:49:01 +0900

    [PG, 42577] 전화번호 목록

--- src/programmers/level2/PG42577.java
+++ src/programmers/level2/PG42577.java
@@ -1,24 +1,46 @@
-package programmers.level2;
+package programmers.level2.success;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-// 전화번호 목록 [해시]
+// 전화번호 목록
 public class PG42577 {
-
     public static void main(String[] args) {
-        boolean solution = solution(new String[]{"222", "111", "332223"});
-        System.out.println("solution = " + solution);
+        boolean solution1 = solution1(new String[]{"119", "97674223", "1195524421"});
+        boolean solution2 = solution2(new String[]{"119", "97674223", "1195524421"});
+        System.out.println("solution2 = " + solution2);
     }
 
-    public static boolean solution(String[] phone_book) {
+    // Hash
+    private static boolean solution2(String[] phone_book) {
+
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < phone_book.length; i++) {
+            map.put(phone_book[i], i);
+        }
+
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                if (map.containsKey(phone_book[i].substring(0, j))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // for문
+    public static boolean solution1(String[] phone_book) {
 
         Arrays.sort(phone_book);
-        // startsWith() 사용해서 접두사 체크 해도됨
         for (int i = 0; i < phone_book.length - 1; i++) {
-            if(phone_book[i + 1].indexOf(phone_book[i]) == 0) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
                 return false;
             }
         }
+
         return true;
     }
 }
