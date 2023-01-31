a113c33b6fc04c6913b33e8764faaced10d78673
ddmkim94 <ddmkim94@gmail.com>
2023-01-31 10:56:40 +0900

    [BOJ, 11650] 좌표 정렬하기

---
+++ baekjoon/sorting/silver/5/BOJ11650.java
@@ -0,0 +1,38 @@
+package baekjoon.sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+// 좌표 정렬하기
+public class BOJ11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] position = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            position[i][0] = Integer.parseInt(st.nextToken());
+            position[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(position, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                }
+
+                return o1[0] - o2[0];
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(position[i][0] + " " + position[i][1]);
+        }
+    }
+}
