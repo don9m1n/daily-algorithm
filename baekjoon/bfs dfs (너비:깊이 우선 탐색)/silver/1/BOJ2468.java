df548ae4f4e0231b4b3938fe6a424351b4fc409a
ddmkim94 <ddmkim94@gmail.com>
2023-05-20 12:44:38 +0900

    [BOJ, 2468] 안전 영역

--- baekjoon/bfs dfs (너비:깊이 우선 탐색)/silver/1/BOJ2468.java
+++ baekjoon/bfs dfs (너비:깊이 우선 탐색)/silver/1/BOJ2468.java
@@ -1,4 +1,4 @@
-package baekjoon.bfsdfs;
+package baekjoon.bfs_dfs;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,19 +8,21 @@ import java.util.*;
 // 안전 영역
 public class BOJ2468 {
 
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     static StringTokenizer st;
-    static int[] dx = {-1, 1, 0, 0}; // 상하
-    static int[] dy = {0, 0, -1, 1}; // 좌우
     static int n;
+    static int[][] map;
+    static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        if(n == 1) return;
 
-        int[][] map = new int[n][n];
+        map = new int[n][n];
+
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
@@ -28,50 +30,55 @@ public class BOJ2468 {
 
         int max = 0;
         int height = 0;
-        while(height <= 100) {
-            boolean[][] visited = new boolean[n][n]; // 방문 지점 체크
+        while (height <= 100) {
+            visited = new boolean[n][n];
             List<Integer> list = new ArrayList<>();
 
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     if (!visited[i][j] && map[i][j] > height) {
-                        list.add(bfs(map, visited, i, j, height));
+                        list.add(bfs(i, j, height));
                     }
                 }
             }
-            if(list.size() == 0) break;
+
+            if(list.size() == 0) {
+                break;
+            }
+
             max = Math.max(max, list.size());
             height++;
         }
 
-        System.out.print(max);
+        System.out.println(max);
     }
 
-    private static int bfs(int[][] map, boolean[][] visited, int x, int y, int h) {
 
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {x, y});
+    static int bfs(int x, int y, int height) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
         visited[x][y] = true;
 
-        int cnt = 1;
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int curX = cur[0];
-            int curY = cur[1];
+        int count = 1;
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int currX = curr[0];
+            int currY = curr[1];
 
             for (int i = 0; i < 4; i++) {
-                int nx = curX + dx[i]; // 다음 x좌표
-                int ny = curY + dy[i]; // 다음 y좌표
+                int nx = currX + dx[i];
+                int ny = currY + dy[i];
 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n) { // 좌표를 옮겼을 때 map을 벗어나지 않는다면
-                    if(!visited[nx][ny] && map[nx][ny] > h) {
-                        q.add(new int[]{nx, ny});
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                    if (!visited[nx][ny] && map[nx][ny] > height) {
+                        queue.add(new int[]{nx, ny});
                         visited[nx][ny] = true;
-                        cnt++;
+                        count++;
                     }
                 }
             }
         }
-        return cnt;
+
+        return count;
     }
 }
