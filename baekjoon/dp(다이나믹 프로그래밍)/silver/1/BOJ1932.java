f703a398ea9c7d17dd3653b41bb0e8f0c2d9028b
ddmkim94 <ddmkim94@gmail.com>
2023-05-24 14:52:15 +0900

    [BOJ, 1932] 정수 삼각형

--- baekjoon/dp(다이나믹 프로그래밍)/silver/1/BOJ1932.java
+++ baekjoon/dp(다이나믹 프로그래밍)/silver/1/BOJ1932.java
@@ -1,4 +1,4 @@
-package baekjoon.dp;
+package baekjoon.dynamic_programming;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,37 +7,28 @@ import java.util.StringTokenizer;
 
 // 정수 삼각형
 public class BOJ1932 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        int[][] dp = new int[N][N];
-        int[][] arr = new int[N][N]; // 가변 배열
+        int[][] dp = new int[n + 1][n + 1];
+        int[][] nums = new int[n + 1][n + 1];
 
         StringTokenizer st;
-        for(int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-
-            for(int j = 0; j < i + 1; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                if(i == N - 1) { // 삼각형의 밑변을 dp 배열에 저장
-                    dp[i][j] = arr[i][j];
-                }
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= i; j++) {
+                nums[i][j] = Integer.parseInt(st.nextToken());
+                if (i == n) dp[i][j] = nums[i][j];
             }
         }
 
-        int result = triangleDP(N, dp, arr);
-        System.out.println("result = " + result);
-    }
-
-    private static int triangleDP(int N, int[][] dp, int[][] arr) {
-        for (int i = N - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                dp[i][j] = arr[i][j] + Math.max(dp[i+1][j], dp[i+1][j+1]);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i + 1][j] + nums[i][j], dp[i + 1][j + 1] + nums[i][j]);
             }
         }
 
-        return dp[0][0];
+        System.out.println(dp[1][1]);
     }
 }
