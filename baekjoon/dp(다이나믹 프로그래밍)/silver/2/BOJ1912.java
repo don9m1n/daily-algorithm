842ec682572f25163c967910ce439ddb091d1689
ddmkim94 <ddmkim94@gmail.com>
2023-05-27 13:28:02 +0900

    [BOJ, 1912] 연속합

---
+++ baekjoon/dp(다이나믹 프로그래밍)/silver/2/BOJ1912.java
@@ -0,0 +1,31 @@
+package baekjoon.dynamic_programming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 연속합
+public class BOJ1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[n + 1];
+        int[] dp = new int[n + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = nums[1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
