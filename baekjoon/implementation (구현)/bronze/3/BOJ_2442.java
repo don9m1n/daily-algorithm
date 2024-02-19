a938e4876cd7ef266cb8483140c1aa41a5572511
Dongmin Kim <ddmkim94@gmail.com>
2024-02-19 11:36:28 +0900

    [BOJ, 2442] 별 찍기5

---
+++ baekjoon/implementation (구현)/bronze/3/BOJ_2442.java
@@ -0,0 +1,23 @@
+package baekjoon.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 별 찍기 5
+public class BOJ_2442 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
