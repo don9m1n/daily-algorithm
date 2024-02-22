c13bbeb94ebbee998bf572e34bed93b09ee69a6f
Dongmin Kim <ddmkim94@gmail.com>
2024-02-22 10:19:31 +0900

    [BOJ, 2075] N번째 큰 수

---
+++ baekjoon/priority queue (우선순위 큐)/silver/2/BOJ_2075.java
@@ -0,0 +1,33 @@
+package baekjoon.priorityqueue.silver2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+// N번째 큰 수
+public class BOJ_2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            while (st.hasMoreElements()) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            pq.poll();
+        }
+
+        System.out.println(pq.poll());
+    }
+}
