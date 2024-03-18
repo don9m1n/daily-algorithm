603648c89823a4e2ad9edcc0ea9fe8dd15789b3f
Dongmin Kim <ddmkim94@gmail.com>
2024-03-18 13:02:01 +0900

    [PG, 135807] 숫자 카드 나누기
    각 배열의 최대 공약수를 구하고, 다른 배열의 요소들이 최대 공약수로 나눠지지 않는지 검사하면 더 빠르게 검사가 가능함.
    최대 공약수는 해당 배열 나눌 수 있는 가장 큰 수이기 때문에 배열을 하나만 검사해서 풀 수 있음

--- programmers/level2/PG_135807.java
+++ programmers/level2/PG_135807.java
@@ -4,6 +4,7 @@ package progammers.level2;
 public class PG_135807 {
     public static void main(String[] args) {
         System.out.println(solution(new int[]{10, 17}, new int[]{5, 20}));
+        System.out.println(solution(new int[]{4, 10, 17}, new int[]{4, 5, 20}));
         System.out.println(solution(new int[]{10, 20}, new int[]{5, 17}));
         System.out.println(solution(new int[]{14, 35, 119}, new int[]{18, 30, 102}));
 
@@ -11,31 +12,42 @@ public class PG_135807 {
 
     static int solution(int[] arrayA, int[] arrayB) {
 
-        int max = Math.max(arrayA[0], arrayB[0]);
+        int gcdA = arrayA[0];
+        int gcdB = arrayB[0];
+
+        // 각 배열의 최대 공약수 구하기
+        for (int i = 1; i < arrayA.length; i++) {
+            gcdA = gcd(arrayA[i], gcdA);
+            gcdB = gcd(arrayB[i], gcdB);
+        }
 
         int answer = 0;
-        for (int i = 2; i <= max; i++) {
-            if (divide(i, arrayA) && notDivide(i, arrayB) || divide(i, arrayB) && notDivide(i, arrayA)) {
-                answer = i;
-            }
+
+        if (notDivide(gcdA, arrayB)) {
+            answer = Math.max(answer, gcdA);
+        }
+
+        if(notDivide(gcdB, arrayA)) {
+            answer = Math.max(answer, gcdB);
         }
 
         return answer;
     }
 
-    private static boolean divide(int max, int[] array) {
-        for (int num : array) {
-            if (num % max > 0) {
-                return false;
-            }
+    private static int gcd(int a, int b) {
+
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b= r;
         }
 
-        return true;
+        return a;
     }
 
-    private static boolean notDivide(int max, int[] array) {
+    private static boolean notDivide(int gcd, int[] array) {
         for (int num : array) {
-            if (num % max == 0) {
+            if (num % gcd == 0) {
                 return false;
             }
         }
