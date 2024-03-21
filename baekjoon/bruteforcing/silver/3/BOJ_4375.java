9cff963862ebee8fc69231555a08eb6e956c242a
Dongmin Kim <ddmkim94@gmail.com>
2024-03-21 12:57:03 +0900

    [BOJ, 4375] 1 수학적으로 풀이

--- baekjoon/bruteforcing/silver/3/BOJ_4375.java
+++ baekjoon/bruteforcing/silver/3/BOJ_4375.java
@@ -3,7 +3,6 @@ package baekjoon.brute_force;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 // 1
 public class BOJ_4375 {
@@ -11,18 +10,20 @@ public class BOJ_4375 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String line;
-        while (!(line = br.readLine()).isEmpty()) {
-            BigInteger n = new BigInteger(line);
-            System.out.println(one(n).length());
-        }
-    }
+        while ((line = br.readLine()) != null) { // EOF: End Of File
 
-    private static String one(BigInteger n) {
-        BigInteger one = BigInteger.ONE;
-        while (one.mod(n).intValue() > 0) {
-            one = (one.multiply(BigInteger.TEN)).add(BigInteger.ONE);
-        }
+            int num = Integer.parseInt(line);
+            int mod = 0;
 
-        return String.valueOf(one);
+            for(int i = 1; ; i++){
+                mod = (mod * 10 + 1) % num;
+
+                if(mod == 0) {
+                    System.out.println(i);
+                    break;
+                }
+            }
+
+        }
     }
 }
